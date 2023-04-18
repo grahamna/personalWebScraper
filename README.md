774c8634d7e8f5b086a21c8197b2c04905e0fab1
grahamna4 <nate.a.graham@gmail.com>
2023-04-17 22:23:34 -0700

    v1 of project webScraper

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # personalWebScraper
- viaPython
+ python --version 3.11.2
+ 
+# Description
+ This Python script allows you to search for a product on eBay and Amazon, and compare the prices and ratings of the product across both websites. You can specify the keyword for the search and the maximum number of pages to scrape. The script stores the data in CSV files named after the searched keyword, for both Amazon and eBay.
+
+ May work on expanding this project to include grocery stores to find the best deals.
+
+# Dependencies
+ tqdm
+ pandas
+ requests-html
+ datetime
+ 
+# Usage
+ Install the required dependencies using the following command: pip install tqdm pandas requests-html datetime
+ Run the script by typing python main.py in the terminal.
+ The script will prompt you to enter the keyword for the product search.
+ The script will then fetch the data from eBay and Amazon, and store it in separate CSV files.
+ The CSV files will be named after the searched keyword, for both Amazon and eBay.
+ You can modify the fetchEbay and fetchAmazon functions to customize the search parameters and URL.
+
+# Contributors
+ Nathan Graham
---
+++ webScraper.py
@@ -0,0 +1,104 @@
+from tqdm import tqdm
+import pandas as pd
+from requests_html import HTMLSession
+import datetime
+
+UserAgent = {
+    "Accept": "application/json",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Dnt": "1",
+    "Connection" : "close",
+    "Upgrade-insecure-requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+  }
+timeStamp = datetime.date.today()
+data = []
+
+
+def fetchEbay(keyword, page_num_max=1, urlString='https://www.ebay.com/sch/i.html?_from=R40&_nkw="{keyword}"&_sacat=0&LH_TitleDesc=0&_fsrp=1&_sop=15&_pgn={page_num}'):
+    session = HTMLSession()
+    urls = []
+    for page_num in range(1, page_num_max+1):
+        urlString = urlString.format(keyword=keyword, page_num=page_num)
+        urls.append(str(urlString))
+    for url in tqdm(urls):
+        response = session.get(url.strip())
+        content = response.html.find('div.s-item__info.clearfix', first=False)
+        count = 0
+        for item in tqdm(content):
+            count = count + 1
+            title = item.find('div.s-item__title', first=True).text
+            try:
+                subtitle = item.find('div.s-item__subtitle', first=True).text
+            except:
+                subtitle = ''
+            price = item.find('span.s-item__price', first=True).text
+            try:
+                shipping = item.find('span.s-item__shipping.s-item__logisticsCost', first=True).text.replace("shipping", "").replace("Free", "")
+                if (shipping == ' '):
+                    shipping = "0"
+            except:
+                shipping = "0"
+            finalPrice = float(price.replace("$", "")) + float(shipping.replace("+$", ""))
+            if(count!=1):
+                data.append([title, subtitle, price, shipping, finalPrice, timeStamp])
+    toDfAndCsvEbay(keyword)
+    
+
+def fetchAmazon(keyword, page_num_max=1, urlString='https://www.amazon.com/s?k={keyword}&s=exact-aware-popularity-rank&dc&page={page_num}'):
+    session = HTMLSession()
+    urls = []
+    for page_num in range(1, page_num_max + 1):
+        urlString = urlString.format(keyword=keyword, page_num=page_num)
+        urls.append(str(urlString))
+    for url in tqdm(urls):
+        response = session.get(url, headers=UserAgent)
+        content = response.html.find('div.a-section.a-spacing-small.a-spacing-top-small', first=False)
+        for item in tqdm(content):
+            record = True
+            if(item != content[0] and item != content[1]):
+                title = item.find('span.a-size-medium', first=True).text
+                try:
+                    ratingCount = item.find('span.a-size-base.s-underline-text', first=True).text
+                    ratingCount = int(ratingCount.replace(',',''))
+                except AttributeError:
+                    ratingCount = ''
+                    record = False
+                try:
+                    price = item.find('span.a-offscreen', first=True).text
+                except AttributeError:
+                    price = ''
+                    record = False
+                if record and ratingCount >= 250:
+                    price = float(price.replace("$", ""))
+                    data.append([title, ratingCount, price, timeStamp])
+    toDfAndCsvAmazon(keyword)
+    
+def quickSearchAndCompare(keyword):
+    fetchEbay(keyword, 1)
+    fetchAmazon(keyword, 1)
+    
+    
+def toDfAndCsvEbay(keyword):
+    df = pd.DataFrame(data, columns=['Title', 'Subtitle', 'Price', 'Shipping', 'Final Price', 'Date'])
+    df.to_csv('{keyword}Ebay.csv'.format(keyword=keyword), index=False,mode="a+")
+    print("Done")
+    data.clear()
+    
+def toDfAndCsvAmazon(keyword):
+    df = pd.DataFrame(data, columns=['Title', 'Rating Count', 'Price', 'Date'])
+    df.to_csv('{keyword}Amazon.csv'.format(keyword=keyword), index=False,mode="a+")
+    print("Done")
+    data.clear()
+        
+        
+def main(*args):
+    fetchEbay("ultimate+hacking+keyboard", 1, 'https://www.ebay.com/sch/i.html?_from=R40&_nkw="{keyword}"&_sacat=0&LH_TitleDesc=0&_fsrp=1&_sop=15&_pgn={page_num}')
+    fetchAmazon("ultimate+hacking+keyboard", 1, 'https://www.amazon.com/s?k={keyword}&s=exact-aware-popularity-rank&dc&page={page_num}')
+    # fetchEbay("graphics+card", 1, 'https://www.ebay.com/sch/i.html?_from=R40&_nkw="{keyword}"&_sacat=0&LH_TitleDesc=0&_fsrp=1&_sop=15&_pgn={page_num}')
+    
+    
+        
+if __name__ == '__main__':
+    main()
